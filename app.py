c48c6bcbf577ab612055059549c7d93c9d0845ed
Ana Murillo <ana_murillo_amores@hotmail.com>
2020-09-26 21:25:28 +0200

    changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+app = Flask("PullRequest")
 @app.route("/")
 def html():
     return '''
@@ -9,12 +10,12 @@ def html():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Neural network tells accents apart</title>
+    <title>Help the instructors</title>
 </head>
 <body style="background-color:#B0C4DE;">
 <h1>Introduce cualquiera de los endpoints incluidos en el README</h1>
 <a href="https://github.com/AnaMA96/Ranking-Project">Github link</a>
-<img src="https://techsalesgroup.files.wordpress.com/2016/10/ironhack-logo-negro1.jpg"/>
+<img src="https://techsalesgroup.files.wordpress.com/2016/10/ironhack-logo-negro1.jpg"/ width="100" height="100">
 <form>
     <input type="button" value="Go back" onclick="history.back()">
 </form>
@@ -22,4 +23,3 @@ def html():
 <center>
 </html>
 '''
-app = Flask("PullRequest")
